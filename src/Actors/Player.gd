a3e0d3a5aa9b2b0383ad9077170bf022b66a135f
VideoJope <jopedrolopes@hotmail.com>
2021-02-20 22:01:22 -0300

    Animação adicionada ao Player
    Reorganização da pasta assets e sprites incluídos.

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -4,21 +4,36 @@ export var moveSpeed: = 600.0
 
 var velocity: = Vector2.ZERO
 
+var _isFullRunning = false
+
 func _physics_process(delta: float) -> void:
-	
+	movement()
+	spriteAnimation()
+
+func movement():
 	var movementDirection: = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
 	
-	velocity = movementDirection * moveSpeed
+	velocity = movementDirection.normalized() * moveSpeed    #normalized movement direction so diagonal movement is not faster than it should be
 	velocity = move_and_slide(velocity)
-	faceMovementDirection()
-	
-	
-func faceMovementDirection() -> void:
-	if velocity.x > 0: 
-		get_node("playerSpriteIdle").set_flip_h(true)
+
+
+func spriteAnimation():
+	if velocity.x > 0:
+		$AnimatedSprite.set_flip_h(true)
 	else:
 		if velocity.x < 0:
-			get_node("playerSpriteIdle").set_flip_h(false)
+			$AnimatedSprite.set_flip_h(false)
+	
+	if velocity.length() > 0:
+		if not _isFullRunning:
+			$AnimatedSprite.play("idleToMoving")
+			if $AnimatedSprite.frame == $AnimatedSprite.frames.get_frame_count("idleToMoving") - 1:
+				_isFullRunning = true
+		else:
+			$AnimatedSprite.play("moving")
+	else:
+		_isFullRunning = false
+		$AnimatedSprite.play("idle")
