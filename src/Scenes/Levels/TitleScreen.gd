93bf9eda1daf4cebf908407571557c9c11c52433
leticiatavaresds <leticiatavares@dcc.ufrj.br>
2021-02-25 21:48:32 -0300

    Menu com som e caixa de seleção para o movimento com mouse

--- src/Scenes/Levels/TitleScreen.gd
+++ src/Scenes/Levels/TitleScreen.gd
@@ -9,9 +9,36 @@ func _ready():
 		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
 		
 func _on_button_pressed(scene_to_load):
+	$Menu/MenuSelect.play()
 	scene_path_to_load = scene_to_load
 	$FadeIn.show()
 	$FadeIn.fade_in()
 
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
+
+
+func _on_CreditsButton_gui_input(event):
+	if event.is_action_pressed("ui_up"):
+		$Menu/MenuMove.play()
+
+func _on_InstructionsButton_gui_input(event):
+	if event.is_action_pressed("ui_up") or event.is_action_pressed("ui_down"):
+		$Menu/MenuMove.play()
+
+func _on_NewGameButton_gui_input(event):
+	if event.is_action_pressed("ui_down"):
+		$Menu/MenuMove.play()
+
+
+func _on_CreditsButton_mouse_entered():
+	$Menu/MenuMove.play()
+	$Menu/CenterRow/Buttons/CreditsButton.grab_focus()
+
+func _on_InstructionsButton_mouse_entered():
+	$Menu/MenuMove.play()
+	$Menu/CenterRow/Buttons/InstructionsButton.grab_focus()
+
+func _on_NewGameButton_mouse_entered():
+	$Menu/MenuMove.play()
+	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
--- src/Scenes/Levels/return_to_title.gd
+++ src/Scenes/Levels/return_to_title.gd
@@ -2,3 +2,12 @@ extends Control
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://src/Scenes/Levels/TitleScreen.tscn")
+
+func _on_Button_button_down():
+	$Credits/MenuSelect.play()
+	
+
+
+
+func _on_Button_ready():
+	$Credits/Button.grab_focus()
